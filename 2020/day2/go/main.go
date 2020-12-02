fccff95369541bb0d339c7a10244fa3e36538352
Jonathan Pentecost <pentecostjonathan@gmail.com>
2020-12-02 08:07:02 -0500

    2020/day2/go: part 1

---
+++ 2020/day2/go/main.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"log"
+)
+
+const ()
+
+func main() {
+	data, err := ioutil.ReadFile("./input.txt")
+	if err != nil {
+		log.Fatalf("unable to open file: %v", err)
+	}
+
+	part1(data)
+}
+
+func part1(data []byte) {
+	lines := bytes.Split(data, []byte{'\n'})
+
+	validPasswords := 0
+	for _, line := range lines {
+		if isValid(line) {
+			fmt.Printf("VALID LINE: %s\n", line)
+			validPasswords += 1
+		} else {
+			fmt.Printf("INVALID LINE: %s\n", line)
+		}
+	}
+	fmt.Printf("valid passwords: %d\n", validPasswords)
+}
+
+type state int
+
+const (
+	state_FirstNumber state = iota
+	state_Dash
+	state_SecondNumber
+	state_Letter
+	state_Word
+)
+
+func isValid(line []byte) bool {
+
+	var min, max int
+	var letter byte
+
+	s := state_FirstNumber
+
+	start := 0
+	for i, c := range line {
+		switch s {
+		case state_FirstNumber:
+			if c == '-' {
+				s = state_Dash
+				min = convertToInt(line[start:i])
+			}
+		case state_Dash:
+			if c >= '0' && c <= '9' {
+				s = state_SecondNumber
+				start = i
+			}
+		case state_SecondNumber:
+			if c == ' ' {
+				s = state_Letter
+				max = convertToInt(line[start:i])
+			}
+		case state_Letter:
+			if c >= 'a' && c <= 'z' {
+				s = state_Word
+				letter = c
+			}
+		case state_Word:
+			if c >= 'a' && c <= 'z' {
+				// validate password
+				count := 0
+				if c == letter {
+					count += 1
+				}
+				for _, c := range line[i+1:] {
+					if c == letter {
+						count += 1
+					}
+				}
+				return min <= count && count <= max
+			}
+		}
+	}
+	return false
+}
+
+func convertToInt(val []byte) int {
+	iVal := 0
+	startBase := 1
+	for i := len(val) - 1; i >= 0; i-- {
+		iVal += startBase * int(val[i]-'0')
+		startBase *= 10
+	}
+	return iVal
+}
