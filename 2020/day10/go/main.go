400de554ef778963c0e32369afb9380af2cf1373
Jonathan Pentecost <pentecostjonathan@gmail.com>
2020-12-11 17:49:33 -0500

    2020/{day10,day11}: part 1 and 2 done

---
+++ 2020/day10/go/main.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"sort"
+)
+
+func main() {
+	data, err := ioutil.ReadFile("./input.txt")
+	if err != nil {
+		log.Fatalf("unable to open file: %v", err)
+	}
+
+	run(data)
+}
+
+func run(data []byte) {
+	lines := bytes.Split(data, []byte{'\n'})
+	numbers := make([]int, 0, len(lines)+1)
+	numbers = append(numbers, 0)
+
+	for _, line := range lines {
+		if len(line) == 0 {
+			continue
+		}
+		numbers = append(numbers, convertToInt(line))
+	}
+
+	sort.Slice(numbers, func(i, j int) bool {
+		return numbers[i] < numbers[j]
+	})
+
+	diffCount := map[int]int{}
+	nextNumbers := map[int][]int{}
+
+	maxJump := 3
+
+	prev := 0
+	for i, num := range numbers {
+		diffCount[num-prev]++
+		prev = num
+
+		for j := 1; j < maxJump+1; j++ {
+			n := i + j
+			if len(numbers) <= n {
+				break
+			}
+			val := numbers[n]
+			if val-num > maxJump {
+				break
+			}
+			nextNumbers[num] = append(nextNumbers[num], val)
+		}
+	}
+
+	fmt.Println(numbers)
+	fmt.Println(len(numbers), diffCount)
+	fmt.Println(diffCount[1] * (diffCount[3] + 1))
+	fmt.Println(nextNumbers)
+
+	total := paths(nextNumbers, 0)
+	fmt.Println(total)
+}
+
+var seen map[string]int
+
+func hash(num, inc int) string {
+	return fmt.Sprintf("%d.%d", num, inc)
+}
+
+func paths(nextNumbers map[int][]int, start int) int {
+	if seen == nil {
+		seen = map[string]int{}
+	}
+	total := 0
+	next := nextNumbers[start]
+	if len(next) == 0 {
+		return 1
+	}
+	for _, n := range next {
+		hn := hash(start, n)
+		t, ok := seen[hn]
+		if !ok {
+			t = paths(nextNumbers, n)
+			seen[hn] = t
+		}
+		total += t
+	}
+	return total
+}
+
+func convertToInt(val []byte) int {
+	iVal := 0
+	startBase := 1
+	for i := len(val) - 1; i >= 0; i-- {
+		iVal += startBase * int(val[i]-'0')
+		startBase *= 10
+	}
+	return iVal
+}
