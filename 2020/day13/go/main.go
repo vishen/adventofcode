753db4dff78cfcf15acd7677bafc494994c45121
Jonathan Pentecost <pentecostjonathan@gmail.com>
2020-12-13 11:58:33 -0500

    2020/day13: part 2 done

--- 2020/day13/go/main.go
+++ 2020/day13/go/main.go
@@ -20,28 +20,73 @@ func run(data []byte) {
 
 	startTime := convertToInt(dataSplit[0])
 
+	max := 0
+	pos := 0
+
 	buses := []int{}
-	for _, b := range bytes.Split(dataSplit[1], []byte{','}) {
+	busPos := map[int]int{}
+
+	for i, b := range bytes.Split(dataSplit[1], []byte{','}) {
 		if b[0] == 'x' {
 			continue
 		}
+		bi := convertToInt(b)
+		buses = append(buses, bi)
+		busPos[bi] = i
 
-		buses = append(buses, convertToInt(b))
+		if max < bi {
+			max = bi
+			pos = i
+		}
 	}
 
 	fmt.Println(startTime, buses)
 
-	nextBus := buses[0]
-	min := buses[0] - (startTime % buses[0])
-	for _, b := range buses[1:] {
-		diff := b - (startTime % b)
-		if diff < min {
-			nextBus = b
-			min = diff
+	// Part 1
+	if false {
+		nextBus := buses[0]
+		min := buses[0] - (startTime % buses[0])
+		for _, b := range buses[1:] {
+			diff := b - (startTime % b)
+			if diff < min {
+				nextBus = b
+				min = diff
+			}
+		}
+		fmt.Println(nextBus, min, nextBus*min)
+	}
+
+	// Part 2
+
+	fmt.Println(max, pos)
+	fmt.Println(busPos)
+
+	if false {
+		// Naive approach, take days
+		offset := 100000000000000
+		starting := offset - (offset % max)
+	Outer:
+		for i := starting; ; i += max {
+			t := i - pos
+			for b, j := range busPos {
+				if (t+j)%b != 0 {
+					continue Outer
+				}
+			}
+			fmt.Println("Found", t)
+			break
 		}
 	}
 
-	fmt.Println(nextBus, min, nextBus*min)
+	t := 0
+	mod := 1
+	for bus, pos := range busPos {
+		for (t+pos)%bus != 0 {
+			t += mod
+		}
+		mod *= bus
+	}
+	fmt.Println(mod, t)
 }
 
 func convertToInt(val []byte) int {
