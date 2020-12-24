da57fc2518fa4bfc38d09f7762f5388c4f86ce37
Jonathan Pentecost <pentecostjonathan@gmail.com>
2020-12-24 08:23:18 -0500

    2020/day24: part 2 working

--- 2020/day24/go/main.go
+++ 2020/day24/go/main.go
@@ -76,11 +76,104 @@ func run(data []byte) {
 	}
 
 	// Part 1
+	total := flippedTiles(tiles)
+	fmt.Println("Flipped:", total)
+
+	// Part 2
+	days := 100
+	for i := 0; i < days; i++ {
+		tiles = update(tiles)
+		total := flippedTiles(tiles)
+		fmt.Println("Day", i+1, ": ", total)
+	}
+}
+
+func flippedTiles(tiles map[dir]bool) int {
 	total := 0
 	for _, flipped := range tiles {
 		if flipped {
 			total += 1
 		}
 	}
-	fmt.Println("Flipped:", total)
+	return total
+}
+
+func update(tiles map[dir]bool) map[dir]bool {
+	newTiles := map[dir]bool{}
+
+	// Handle black tiles
+	for tile, ok := range tiles {
+		if !ok {
+			continue
+		}
+		adjacent := 0
+		for _, d := range knownDirections {
+			if f := tiles[dir{tile.row + d.row, tile.col + d.col}]; f {
+				adjacent += 1
+			}
+		}
+
+		if adjacent > 0 && adjacent <= 2 {
+			newTiles[tile] = true
+		}
+	}
+
+	// Handle white tiles
+	whiteTiles := []dir{}
+	for tile, ok := range tiles {
+		if !ok {
+			continue
+		}
+		for _, d := range knownDirections {
+			dir := dir{tile.row + d.row, tile.col + d.col}
+			f := tiles[dir]
+			if !f {
+				whiteTiles = append(whiteTiles, dir)
+			}
+		}
+	}
+
+	for _, tile := range whiteTiles {
+		adjacent := 0
+		for _, d := range knownDirections {
+			if f := tiles[dir{tile.row + d.row, tile.col + d.col}]; f {
+				adjacent += 1
+			}
+		}
+
+		if adjacent == 2 {
+			newTiles[tile] = true
+		}
+	}
+
+	return newTiles
+}
+
+// Naive approach
+func updateNaive(tiles map[dir]bool) map[dir]bool {
+	newTiles := map[dir]bool{}
+	size := len(tiles)
+	for i := -size; i < size; i++ {
+		for j := -size; j < size; j++ {
+			flipped := tiles[dir{i, j}]
+
+			adjacent := 0
+			for _, d := range knownDirections {
+				if f := tiles[dir{i + d.row, j + d.col}]; f {
+					adjacent += 1
+				}
+			}
+
+			if flipped {
+				if adjacent > 0 && adjacent <= 2 {
+					newTiles[dir{i, j}] = true
+				}
+			} else {
+				if adjacent == 2 {
+					newTiles[dir{i, j}] = true
+				}
+			}
+		}
+	}
+	return newTiles
 }
